6f55db0215570ce634a7d9ecd794936f7af13634
halilyurdusev <64197454+halilyurdusev@users.noreply.github.com>
2022-06-10 15:00:46 +0300

    Initial commit

---
+++ names.php
@@ -0,0 +1,17 @@
+<?php
+
+class nameGiver{
+    private $age;
+    public function __construct($age)
+    {
+        $this->age = $age;
+
+    }
+    function getName($name)
+    {
+        return "my name is " . $name . "and age is " . $this->age;
+    }
+}
+
+ 
+?>
