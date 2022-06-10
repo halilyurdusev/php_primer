07be8986ec123d34a75d8f4e278bc4a1a77e9a34
halilyurdusev <64197454+halilyurdusev@users.noreply.github.com>
2022-06-10 15:02:48 +0300

    new lines

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ require "names.php";
 
 </div>
 
-<!-- JavaScript Bundle with Popper -->
+<!-- JavaScript Bundle with Popperü
+new line -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
